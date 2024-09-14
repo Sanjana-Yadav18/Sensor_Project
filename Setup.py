e302f45f1a8a9e239b5cc89afa65b68002ab0bee
Your NameSanjana <you@example.com>
2024-09-14 16:35:21 +0530

    this is my first commit

--- Setup.py
+++ Setup.py
@@ -1,18 +1,23 @@
-from setuptools import find_packages,setup
-from typing import List
+from setuptools import find_packages , setup
+from typing import List 
 
-def get_reqiurements(file_path: str) -> list[str]:
-    requirements = []
-    with open (file_path) as file_obj:
-        requirements  =file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
-        return requirements 
+HYPEN_E_DOT= '-e.'
+
+def get_requirements(file_path:str) ->List [str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
+
     name='Fault Detection',
-    version= '0.0.1',
-    author = 'Sanjana',
-    author_email= 'sanjanaydv3@gmail.com',
-    install_requirements = get_reqiurements('requirements.txt'),
-    packages= find_packages()
+    version='0.0.1',
+    author='sanjana',
+    author_email='sanjanaydv3@gmail.com',
+    install_requirements=get_requirements('requirements.txt'),
+    packages=find_packages()
 )
